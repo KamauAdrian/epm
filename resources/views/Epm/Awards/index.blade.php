8ff481a93cb79dfef2742e5cb5ea0f4449610688
KamauAdrian <adriankubai1999@gmail.com>
2021-02-24 13:00:54 +0300

    updated wednessday 23rd feb 2021 @1235 awards index view/listing update

--- resources/views/Epm/Awards/index.blade.php
+++ resources/views/Epm/Awards/index.blade.php
@@ -33,9 +33,11 @@
                             <table id="AwardsListTable" class="table table-center mb-0 ">
                                 <thead>
                                     <tr>
-                                        <th>Awards</th>
-                                        <th>Award Winners</th>
-                                        <th class="text-right">Actions</th>
+                                        <th>Award</th>
+                                        <th>Position One</th>
+                                        <th>Position Two</th>
+                                        <th>Position Three</th>
+{{--                                        <th class="text-right">Actions</th>--}}
                                     </tr>
                                 </thead>
                                 @if($awards!='')
@@ -52,49 +54,136 @@
                                                 </div>
                                             </td>
                                             <td>
-                                                <p>
                                                     @if($award->position_one)
                                                         <?php
-                                                        $winner_position_one = \App\Models\User::find($award->position_one)
+                                                        $winner_position_one = \App\Models\User::find($award->position_one);
+                                                        $image = '';
+                                                        $profile_image = $winner_position_one->image;
+                                                        if ($profile_image==null){
+                                                            $gender = $winner_position_one->gender;
+                                                            if ($gender=="Male"){
+                                                                $image = 'assets/images/male.jpeg';
+                                                            }else{
+                                                                $image = 'assets/images/female.jpeg';
+                                                            }
+                                                        }else{
+                                                            $role = $winner_position_one->role->name;
+                                                            if ($role=="Center Manager"){
+                                                                $image = "CenterManagers/images/".$profile_image;
+                                                            }
+                                                            if ($role=="Project Manager"){
+                                                                $image = "ProjectManagers/images/".$profile_image;
+                                                            }
+                                                        }
                                                         ?>
-                                                         Position One:  <b>{{$winner_position_one->name}}</b> <br />
                                                     @endif
+                                                    <div class="media">
+                                                        <img src="{{url($image)}}" alt="images" class="img-fluid avtar avtar-s">
+                                                        <div class="media-body ml-3 align-self-center">
+                                                            <h5 class="mb-1">{{$winner_position_one->name}}</h5>
+{{--                                                                <p class="mb-0">Chicago</p>--}}
+                                                        </div>
+                                                    </div>
+                                            </td>
+                                            <td>
                                                     @if($award->position_two)
                                                         <?php
-                                                        $winner_position_two = \App\Models\User::find($award->position_two)
-                                                        ?>
-                                                        Position Two:  <b>{{$winner_position_two->name}}</b> <br />
-                                                    @endif
+                                                        $winner_position_two = \App\Models\User::find($award->position_two);
+                                                    $image = '';
+                                                    $profile_image = $winner_position_two->image;
+                                                    if ($profile_image==null){
+                                                        $gender = $winner_position_two->gender;
+                                                        if ($gender=="Male"){
+                                                            $image = 'assets/images/male.jpeg';
+                                                        }else{
+                                                            $image = 'assets/images/female.jpeg';
+                                                        }
+                                                    }else{
+                                                        $role = $winner_position_two->role->name;
+                                                        if ($role=="Center Manager"){
+                                                            $image = "CenterManagers/images/".$profile_image;
+                                                        }
+                                                        if ($role=="Project Manager"){
+                                                            $image = "ProjectManagers/images/".$profile_image;
+                                                        }
+                                                    }
+                                                    ?>
+                                                @endif
+                                                <div class="media">
+                                                    <img src="{{url($image)}}" alt="images" class="img-fluid avtar avtar-s">
+                                                    <div class="media-body ml-3 align-self-center">
+                                                        <h5 class="mb-1">{{$winner_position_two->name}}</h5>
+                                                        {{--                                                                <p class="mb-0">Chicago</p>--}}
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
                                                     @if($award->position_three)
                                                         <?php
-                                                        $winner_position_three = \App\Models\User::find($award->position_three)
-                                                        ?>
-                                                        Position Two:  <b>{{$winner_position_three->name}}</b> <br />
-                                                    @endif
-                                                </p>
+                                                        $winner_position_three = \App\Models\User::find($award->position_three);
+                                                    $image = '';
+                                                    $profile_image = $winner_position_three->image;
+                                                    if ($profile_image==null){
+                                                        $gender = $winner_position_three->gender;
+                                                        if ($gender=="Male"){
+                                                            $image = 'assets/images/male.jpeg';
+                                                        }else{
+                                                            $image = 'assets/images/female.jpeg';
+                                                        }
+                                                    }else{
+                                                        $role = $winner_position_three->role->name;
+                                                        if ($role=="Center Manager"){
+                                                            $image = "CenterManagers/images/".$profile_image;
+                                                        }
+                                                        if ($role=="Project Manager"){
+                                                            $image = "ProjectManagers/images/".$profile_image;
+                                                        }
+                                                    }
+                                                    ?>
+                                                @endif
+                                                <div class="media">
+                                                    <img src="{{url($image)}}" alt="images" class="img-fluid avtar avtar-s">
+                                                    <div class="media-body ml-3 align-self-center">
+                                                        <h5 class="mb-1">{{$winner_position_three->name}}</h5>
+                                                        {{--                                                                <p class="mb-0">Chicago</p>--}}
+                                                    </div>
+                                                </div>
                                             </td>
-                                            @if($auth_admin->role->name != 'Su Admin')
-                                                <td class="text-right">
-                                                    <div class="float-right">
-                                                        <a href="{{url('/adm/'.$auth_admin->id.'/view/award/'.$award->id)}}" class="btn btn-sm btn-outline-info" title="View">
-                                                            <span><i class="fa fa-list"></i></span>
-                                                        </a>
-                                                    </div>
-                                                </td>
-                                            @else
-                                                <td class="text-right">
-                                                    <div class="float-right">
-                                                        <a href="{{url('/adm/'.$auth_admin->id.'/view/award/'.$award->id)}}" class="btn btn-sm btn-outline-info" title="View">
-                                                            <span><i class="fa fa-list"></i></span>
-                                                        </a>
-                                                        <a href="{{url('/adm/'.$auth_admin->id.'/edit/award/'.$award->id)}}" class="btn btn-sm btn-outline-info" title="Edit">
-                                                            <span><i class="fa fa-pencil-alt"></i></span>
-                                                        </a>
-                                                        <div  data-url="{{url('/adm/'.$auth_admin->id.'/delete/award/'.$award->id)}}" class="btn btn-sm btn-outline-danger deleteAdmin" data-toggle="modal"  title="Delete">
-                                                            <span><i class="fa fa-trash"></i></span>
-                                                    </div>
-                                                </td>
-                                            @endif
+
+{{--                                            @if($auth_admin->role->name != 'Su Admin')--}}
+{{--                                                <td class="text-right">--}}
+{{--                                                    <div class="float-right">--}}
+{{--                                                        <a href="{{url('/adm/'.$auth_admin->id.'/view/award/'.$award->id)}}" class="btn btn-sm btn-outline-info" title="View">--}}
+{{--                                                            <span><i class="fa fa-list"></i></span>--}}
+{{--                                                        </a>--}}
+{{--                                                    </div>--}}
+{{--                                                </td>--}}
+{{--                                            @else--}}
+{{--                                                <td class="text-right">--}}
+{{--                                                    <div class="float-right">--}}
+{{--                                                        <a href="#!" class="btn btn-sm btn-outline-info" title="View">--}}
+{{--                                                            <span><i class="fa fa-list"></i></span>--}}
+{{--                                                        </a>--}}
+{{--                                                        <a href="#!" class="btn btn-sm btn-outline-info" title="Edit">--}}
+{{--                                                            <span><i class="fa fa-pencil-alt"></i></span>--}}
+{{--                                                        </a>--}}
+{{--                                                        <div  data-url="#!" class="btn btn-sm btn-outline-danger deleteAdmin" data-toggle="modal"  title="Delete">--}}
+{{--                                                            <span><i class="fa fa-trash"></i></span>--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
+{{--                                                    <div class="float-right">--}}
+{{--                                                        <a href="{{url('/adm/'.$auth_admin->id.'/view/award/'.$award->id)}}" class="btn btn-sm btn-outline-info" title="View">--}}
+{{--                                                            <span><i class="fa fa-list"></i></span>--}}
+{{--                                                        </a>--}}
+{{--                                                        <a href="{{url('/adm/'.$auth_admin->id.'/edit/award/'.$award->id)}}" class="btn btn-sm btn-outline-info" title="Edit">--}}
+{{--                                                            <span><i class="fa fa-pencil-alt"></i></span>--}}
+{{--                                                        </a>--}}
+{{--                                                        <div  data-url="{{url('/adm/'.$auth_admin->id.'/delete/award/'.$award->id)}}" class="btn btn-sm btn-outline-danger deleteAdmin" data-toggle="modal"  title="Delete">--}}
+{{--                                                            <span><i class="fa fa-trash"></i></span>--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
+{{--                                                </td>--}}
+{{--                                            @endif--}}
                                         </tr>
                                     @endforeach
                                     </tbody>
